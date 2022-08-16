7696eb0aeeb8dab3de1f67ad076f8f0b04f53075
Toby Corkindale <tjc@wintrmute.net>
2022-08-17 00:28:54 +1000

    stub of LoRaDriver

---
+++ src/loradriver.py
@@ -0,0 +1,18 @@
+import machine
+import uasyncio as asyncio
+
+# Lilygo LoRa32 T3 V1.6.1
+# LED 25
+
+# LoRa pins:
+# CS 18
+# SCLK 5
+# MOSI 27 (SDI)
+# MISO 19 (SDO)
+# DIO0 26
+# RST 23
+
+
+class LoRaDriver:
+    def todo():
+        return
