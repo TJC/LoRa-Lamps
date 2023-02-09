706aa80cd59eb580bcca15715d8cb86fd152eb7d
Toby Corkindale <tjc@wintrmute.net>
2023-02-09 16:03:33 +1100

    improve C3 mini flash scripts

--- firmware/wemos-c3-mini/erase.sh
+++ firmware/wemos-c3-mini/erase.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+x=$(ls -1 /dev/cu.usb*|head -n 1)
 echo "Hold button 9, press button RST, release button 9"
 sleep 2
-esptool.py --chip esp32-c3 --port /dev/cu.usbmodem2101 erase_flash
+esptool.py --chip esp32-c3 --port "$x" erase_flash
--- firmware/wemos-c3-mini/flash.sh
+++ firmware/wemos-c3-mini/flash.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+x=$(ls -1 /dev/cu.usb*|head -n 1)
 echo "Hold button 9, press button RST, release button 9"
 sleep 2
-esptool.py --chip esp32-c3 --port /dev/cu.usbmodem2101 --baud 1000000 \
+esptool.py --chip esp32-c3 --port "$x" --baud 1000000 \
 	write_flash -z 0 \
 	firmware-lolin-c3-mini-espnow-v1.19.1.bin 
