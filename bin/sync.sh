4b7156db65365b40b8d3eb07031fdc7d30052059
Toby Corkindale <tjc@wintrmute.net>
2023-03-28 00:03:51 +1100

    Sync to all connected boards, not just the first

--- bin/sync.sh
+++ bin/sync.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-x=$(ls -1 /dev/cu.usb*|head -n 1)
-export RSHELL_PORT=$x
-pushd src
-rshell rsync . /pyboard/
-popd
+x=$(ls -1 /dev/cu.usb*)
+for port in $x; do
+  export RSHELL_PORT="$port"
+  pushd src
+  rshell rsync . /pyboard/
+  popd
+done
